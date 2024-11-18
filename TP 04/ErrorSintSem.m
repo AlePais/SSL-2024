456c8958c396a9fdd5940fa60c6636d2228837cb
ConSabino <83124573+ConSabino@users.noreply.github.com>
2024-11-18 11:59:39 -0300

    Update ErrorSintSem.m

--- TP 04/ErrorSintSem.m
+++ TP 04/ErrorSintSem.m
@@ -1,6 +1,6 @@
 inicio
-  leer (a, b, c);
-  var1 := a - b;
-  var1 := 10;
-  escribir (var1, var1);
+  leer (a,b);
+  var := a-b;
+  variableConUnNombreDeMasDe32Caracteres := 10;
+  escribir (variableConUnNombreDeMasDe32Caracteres);
   
